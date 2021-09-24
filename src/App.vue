ae1805dd3ea88a40bf5b933c5ce00d4946eae72d
Henrik Reponen <henrik@rundit.com>
2021-09-24 16:25:17 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>Rundit Assignment</h1>
+    <p>
+      Read the `README.md` to get started with the assignment and happy coding!
+      Investment data can be accessed via the InvestmentService and company data
+      from the CompanyService.
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
